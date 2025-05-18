c6501e04f4e06416da5dd05d7aad0939f76cab9f
QuietLamb <chhunchannygod@gmail.com>
2025-05-18 16:41:34 +0700

    feature issue#3: Add Event Listener for button

--- scipt.js
+++ scipt.js
@@ -3,3 +3,52 @@ const expressionEl = document.querySelector(".expression");
 const resultEl = document.querySelector(".result");
 const buttons = document.querySelectorAll("button");
 let expression = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      const value = button.textContent;
+   
+   
+      switch (value) {
+        case "C":
+          clearExpression();
+          break;
+   
+   
+        case "⌫":
+          backspace();
+          break;
+   
+   
+        case "=":
+          evaluateExpression();
+          break;
+   
+   
+        case "%":
+          applyPercentage();
+          break;
+   
+   
+        case "≠":
+          toggleNegative();
+          break;
+   
+   
+        case "^":
+          handleExponent();
+          break;
+   
+   
+        case "√":
+          handleSquareRoot();
+          break;
+   
+   
+        default:
+          appendToExpression(value);
+          break;
+      }
+    });
+   });
+   
