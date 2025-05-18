1195f4f7f133909354ac2e2d597ba88e39477989
Gony-Rida-ANB <ridagony@gmail.com>
2025-05-18 16:17:03 +0700

    feature issue #3 Element References and Event Listener for Buttons

---
+++ scipt.js
@@ -0,0 +1,5 @@
+// === Module: Element References ===  Rida1 (feature)
+const expressionEl = document.querySelector(".expression");
+const resultEl = document.querySelector(".result");
+const buttons = document.querySelectorAll("button");
+let expression = "";
