56c7dd344af1840c97c763b00d25c032d2b96eb1
QuietLamb <chhunchannygod@gmail.com>
2025-05-18 16:10:36 +0700

    feature issue#5: Apply function Percentage

---
+++ scipt.js
@@ -0,0 +1,10 @@
+function applyPercentage() {
+    const match = expression.match(/(\d+\.?\d*)$/);
+    if (match) {
+      const number = parseFloat(match[1]);
+      const percentValue = number / 100;
+      expression = expression.replace(/(\d+\.?\d*)$/, percentValue.toString());
+      updateDisplay();
+    }
+  }
+ 
